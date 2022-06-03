bbbedbad0b63859e97b9c5d80560c8df7c56dfb1
Janvi <yami.janvi@gmail.com>
2022-06-03 20:16:29 +0530

    some changes

--- src/pages/VisitorPass.jsx
+++ src/pages/VisitorPass.jsx
@@ -46,7 +46,7 @@ const VisitorPass = () => {
                                     </div>
                                 </div>
                             </div>
-                            <table className='table-hover table display expandable-table datatable' id='table'>
+                            <table className='table-hover table display expandable-table datatable ' id='data-table'>
                                 <thead>
                                     <tr>
                                         <th style={{ whiteSpace: "nowrap" }}>No</th>
